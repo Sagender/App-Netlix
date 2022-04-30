e7a47d095a9584f9aafed8a7376adeb02d7515f4
sagender <44262173@certus.edu.pe>
2022-04-30 14:17:56 -0500

    Trabajando con columnas

---
+++ netflix_interfaz/lib/pages/inicio.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class InicioPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            height: 100.0,
+            width: 100.0,
+          ),
+          Container(
+            color: Colors.red,
+            height: 100.0,
+            width: 100.0,
+          ),
+          Container(
+            color: Colors.green,
+            height: 100.0,
+            width: 100.0,
+          )
+        ],
+      ),
+    );
+  }
+}
