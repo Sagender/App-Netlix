a20a8346b45e5648f15aa39c085e5d12ab96a0ba
sagender <44262173@certus.edu.pe>
2022-04-30 16:31:57 -0500

    Cabecera terminada

--- netflix_interfaz/lib/componentes/cartel_principal.dart
+++ netflix_interfaz/lib/componentes/cartel_principal.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'nav_bar_superior.dart';
 
@@ -6,26 +8,38 @@ class CartelPrincipal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Stack(
-          children: <Widget>[
-            Image.network(
-              'https://assets.puzzlefactory.pl/puzzle/259/492/original.jpg',
-              height: 350.0,
-              fit: BoxFit
-                  .cover, //Permite hacer zoom y que la imagen no salga de su contenedor
-            ),
-            Container(
-              // se agrega para dar efectos de opacidad
-              width: double.infinity,
-              height: 350.0,
-              color: Colors.black45,
-            ),
-            SafeArea(
-                child:
-                    NavBarSuperior()), //Permite el ajuste de la barra de navegación hacia abajo
-          ],
-        )
+        this.cabecera(), // enlace de la función
       ],
     );
   }
+}
+
+//Cambie Stack dentro de una función y agregué this.cabecera
+Widget cabecera() {
+  //Esta función permite enlazar
+  return Stack(
+    children: <Widget>[
+      Image.network(
+        'https://assets.puzzlefactory.pl/puzzle/259/492/original.jpg',
+        height: 350.0,
+        fit: BoxFit
+            .cover, //Permite hacer zoom y que la imagen no salga de su contenedor
+      ),
+      Container(
+        // se agrega para dar efectos de opacidad
+        width: double.infinity,
+        height: 350.0,
+        //color: Colors.black45,(opacidad)
+        decoration: BoxDecoration(
+            // permite hacer el efecto de degradar
+            gradient: LinearGradient(
+                begin: Alignment.center,
+                end: Alignment.bottomCenter, //indica donde inicia el degradado
+                colors: <Color>[Colors.black38, Colors.black])),
+      ),
+      SafeArea(
+          child:
+              NavBarSuperior()), //Permite el ajuste de la barra de navegación hacia abajo
+    ],
+  );
 }
